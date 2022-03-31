d8beacb2b8ef720f94d1f7d28818bcf0da6d0826
Matt Cadorette <matthew.cadorette@lacework.net>
2022-03-31 17:46:54 -0400

    fix(ec2instance): export security group id, keypair, and instance id (#17)

--- extensions/ec2instance/main.tf
+++ extensions/ec2instance/main.tf
@@ -79,3 +79,15 @@ output "pem" {
   value     = tls_private_key.keypair.private_key_pem
   sensitive = true
 }
+
+output "sg_id" {
+  value = aws_security_group.ingress-from-all.id
+}
+
+output "instance_id" {
+  value = aws_instance.instance-server.id
+}
+
+output "keypair" {
+  value = aws_key_pair.server-key.id
+}
