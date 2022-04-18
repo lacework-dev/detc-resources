15b8585b8b469fdedcb84dbf8ece2cf9138c4967
ipcrm <matthew.cadorette@lacework.net>
2022-04-18 15:40:14 -0400

    feat(lacework-accounts): integration lacework into csp accounts

---
+++ apps/lacework-accounts/aws/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    lacework = {
+      source  = "lacework/lacework"
+      version = "~> 0.16"
+    }
+  }
+}
+
+variable "lacework_account" {}
+variable "lacework_subaccount" {}
+variable "lacework_apikey" {}
+variable "lacework_apisecret" {}
+
+provider "lacework" {
+  subaccount = var.lacework_subaccount
+  account    = var.lacework_account
+  api_key    = var.lacework_apikey
+  api_secret = var.lacework_apisecret
+}
+
+provider "aws" {}
+
+module "aws_config" {
+  source  = "lacework/config/aws"
+  version = "~> 0.5"
+}
+
+module "main_cloudtrail" {
+  cloudtrail_name       = "cloudtrail-${var.lacework_subaccount}"
+  source                = "lacework/cloudtrail/aws"
+  version               = "~> 2.0"
+  iam_role_arn          = module.aws_config.iam_role_arn
+  iam_role_external_id  = module.aws_config.external_id
+  iam_role_name         = module.aws_config.iam_role_name
+  use_existing_iam_role = true
+  bucket_force_destroy  = true
+}
---
+++ apps/lacework-accounts/azure/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.16"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.91.0"
+    }
+    lacework = {
+      source  = "lacework/lacework"
+      version = "~> 0.16"
+    }
+  }
+}
+
+variable "lacework_account" {}
+variable "lacework_subaccount" {}
+variable "lacework_apikey" {}
+variable "lacework_apisecret" {}
+
+provider "lacework" {
+  subaccount = var.lacework_subaccount
+  account    = var.lacework_account
+  api_key    = var.lacework_apikey
+  api_secret = var.lacework_apisecret
+}
+
+provider "azuread" {
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+module "az_ad_application" {
+  source  = "lacework/ad-application/azure"
+  version = "~> 1.0"
+}
+
+module "az_config" {
+  source                      = "lacework/config/azure"
+  version                     = "~> 1.0"
+  application_id              = module.az_ad_application.application_id
+  application_password        = module.az_ad_application.application_password
+  service_principal_id        = module.az_ad_application.service_principal_id
+  use_existing_ad_application = true
+}
+
+module "az_activity_log" {
+  source                      = "lacework/activity-log/azure"
+  version                     = "~> 1.0"
+  application_id              = module.az_ad_application.application_id
+  application_password        = module.az_ad_application.application_password
+  service_principal_id        = module.az_ad_application.service_principal_id
+  use_existing_ad_application = true
+}
