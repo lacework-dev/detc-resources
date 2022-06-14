adfb1beedecb6ed575c15052dc90cb3886e1e312
ipcrm <matthew.cadorette@lacework.net>
2022-06-13 21:40:34 -0400

    maint(eks): update eks version and supporting tf

--- extensions/eks/main.tf
+++ extensions/eks/main.tf
@@ -2,8 +2,8 @@ variable "aws_region" {
   description = "AWS region.  Example: us-east-2"
 }
 
-variable "deployment_name" {
-  description = "Name of deployment - used for the cluster name.  Example: rotate"
+variable "cluster_name" {
+  description = "Name of the EKS cluster.  Example: rotate"
 }
 
 provider "kubernetes" {
@@ -11,7 +11,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
-
 
 terraform {
   required_providers {
@@ -44,41 +43,31 @@ provider "aws" {
 }
 
 locals {
-  cluster_name = "eks-demo-${var.deployment_name}"
+  cluster_name = "eks-demo-${var.cluster_name}"
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
+  version         = "18.23.0"
   cluster_name    = local.cluster_name
-  cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+  cluster_version = "1.21"
 
-  tags = {
-    Environment       = "demo"
-    KubernetesCluster = local.cluster_name
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    disk_size      = 50
+    instance_types = ["t2.medium"]
   }
 
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
+  eks_managed_node_groups = {
+    primary = {
+      vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+      min_size               = 1
+      desired_size           = 2
+      max_size               = 10
+    }
   }
-
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_security_group_ids = [aws_security_group.security-group-ssh-1.id]
-      asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_security_group_ids = [aws_security_group.security-group-ssh-2.id]
-      asg_desired_capacity          = 1
-    },
-  ]
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -89,50 +78,27 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-resource "aws_security_group" "security-group-ssh-1" {
-  name_prefix = "security-group-ssh-1"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
-}
-
-resource "aws_security_group" "security-group-ssh-2" {
-  name_prefix = "security-group-ssh-2"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
-  }
-}
-
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
-    ]
+    from_port = 0
+    to_port   = 0
+    protocol  = -1
+    self      = true
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
 
@@ -142,14 +108,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "${local.cluster_name}-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  name                       = "${local.cluster_name}-vpc"
+  cidr                       = "10.0.0.0/16"
+  azs                        = data.aws_availability_zones.available.names
+  private_subnets            = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets             = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway         = true
+  single_nat_gateway         = true
+  enable_dns_hostnames       = true
+  manage_default_network_acl = true
 
   tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
