d00178d14caac6790d4300a6d16861520c88a374
ipcrm <matthew.cadorette@lacework.net>
2022-07-01 16:30:43 -0400

    fix(eks): allow outbound traffic from workers

--- extensions/eks/main.tf
+++ extensions/eks/main.tf
@@ -98,7 +98,7 @@ resource "aws_security_group" "all_worker_mgmt" {
     from_port   = 0
     to_port     = 0
     protocol    = -1
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
