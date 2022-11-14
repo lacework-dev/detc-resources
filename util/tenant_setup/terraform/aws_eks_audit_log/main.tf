61a7426ddac18915fd6d6b24f9d7e591bc1841f0
Gabe O'Brien <gabe.obrien@lacework.net>
2022-11-14 13:58:55 -0800

    feat: adds plan to manage lacework eks audit log intergration setup

---
+++ util/tenant_setup/terraform/aws_eks_audit_log/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    lacework = {
+      source  = "lacework/lacework"
+      version = "1.0.1"
+    }
+  }
+}
+
+variable "lacework_account" {}
+variable "lacework_subaccount" {}
+variable "lacework_apikey" {}
+variable "lacework_apisecret" {}
+variable "cluster_name" {}
+variable "cluster_region" {}
+
+provider "lacework" {
+  subaccount = var.lacework_subaccount
+  account    = var.lacework_account
+  api_key    = var.lacework_apikey
+  api_secret = var.lacework_apisecret
+}
+
+provider "aws" {
+    version = "4.38.0"
+}
+
+module "aws_config" {
+  source  = "lacework/config/aws"
+  version = "0.7.1"
+}
+
+module "aws_eks_audit_log" {
+  source             = "lacework/eks-audit-log/aws"
+  version            = "0.4.2"
+  cloudwatch_regions = [var.cluster_region]
+  cluster_names      = [var.cluster_name]
+}
