2e27703eb1153a7fbb9f3d6799ee9606fa55755f
Gabe O'Brien <gabe.obrien@lacework.net>
2022-12-07 17:45:22 -0800

    fix: pin azure lacework terraform versions

--- apps/lacework-accounts/azure/main.tf
+++ apps/lacework-accounts/azure/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.16"
+      version = "~> 2.31"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.91.0"
+      version = "2.91.0"
     }
     lacework = {
       source  = "lacework/lacework"
-      version = "~> 1.0.1"
+      version = "1.0.1"
     }
   }
 }
@@ -37,12 +37,12 @@ provider "azurerm" {
 
 module "az_ad_application" {
   source  = "lacework/ad-application/azure"
-  version = "~> 1.2.1"
+  version = "1.2.1"
 }
 
 module "az_config" {
   source                      = "lacework/config/azure"
-  version                     = "~> 1.1.1"
+  version                     = "1.1.1"
   application_id              = module.az_ad_application.application_id
   application_password        = module.az_ad_application.application_password
   service_principal_id        = module.az_ad_application.service_principal_id
@@ -51,7 +51,7 @@ module "az_config" {
 
 module "az_activity_log" {
   source                      = "lacework/activity-log/azure"
-  version                     = "~> 1.2.1"
+  version                     = "1.2.1"
   application_id              = module.az_ad_application.application_id
   application_password        = module.az_ad_application.application_password
   service_principal_id        = module.az_ad_application.service_principal_id
