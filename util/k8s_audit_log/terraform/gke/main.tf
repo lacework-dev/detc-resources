ec5765caf758f041cb029bf78a8a08eb67e99534
Matt Cadorette <matthew.cadorette@lacework.net>
2023-01-31 11:03:43 -0500

    fix(gke-audit-logs): fix tf version

--- util/k8s_audit_log/terraform/gke/main.tf
+++ util/k8s_audit_log/terraform/gke/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     lacework = {
       source  = "lacework/lacework"
-      version = "~> 0.26"
+      version = "~> 1.0.1"
     }
     google = {
       source  = "hashicorp/google"
