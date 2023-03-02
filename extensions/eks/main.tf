4ac6370ea99d5d5b6beebedcf2527fb90e817d15
Matt Cadorette <matthew.cadorette@lacework.net>
2023-03-02 11:22:54 -0500

    fix(eks): bump to supported version
    Version 1.21 is deprecated, moving to 1.23

--- extensions/eks/main.tf
+++ extensions/eks/main.tf
@@ -55,7 +55,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "18.26.5"
   cluster_name    = local.cluster_name
-  cluster_version = "1.21"
+  cluster_version = "1.23"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
