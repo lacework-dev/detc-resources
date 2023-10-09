fa3a11519af808034f6730d6f7aaad87e3bbd585
ipcrm <matthew.cadorette@lacework.net>
2023-10-09 08:45:27 -0400

    fix(integration): support multiple account ids for aws

--- apps/lacework-accounts/aws/main.tf
+++ apps/lacework-accounts/aws/main.tf
@@ -11,6 +11,9 @@ variable "lacework_account" {}
 variable "lacework_subaccount" {}
 variable "lacework_apikey" {}
 variable "lacework_apisecret" {}
+variable "lacework_aws_account_id" {
+  default = "434813966438"
+}
 
 provider "lacework" {
   subaccount = var.lacework_subaccount
@@ -22,17 +25,19 @@ provider "lacework" {
 provider "aws" {}
 
 module "aws_config" {
-  source  = "lacework/config/aws"
-  version = "0.7.1"
+  source                  = "lacework/config/aws"
+  version                 = "0.13.0"
+  lacework_aws_account_id = format("%d", var.lacework_aws_account_id)
 }
 
 module "main_cloudtrail" {
-  cloudtrail_name       = "cloudtrail${var.lacework_account}${var.lacework_subaccount}"
-  source                = "lacework/cloudtrail/aws"
-  version               = "2.5.3"
-  iam_role_arn          = module.aws_config.iam_role_arn
-  iam_role_external_id  = module.aws_config.external_id
-  iam_role_name         = module.aws_config.iam_role_name
-  use_existing_iam_role = true
-  bucket_force_destroy  = true
+  cloudtrail_name         = "cloudtrail${var.lacework_account}${var.lacework_subaccount}"
+  source                  = "lacework/cloudtrail/aws"
+  version                 = "2.8.0"
+  iam_role_arn            = module.aws_config.iam_role_arn
+  iam_role_external_id    = module.aws_config.external_id
+  iam_role_name           = module.aws_config.iam_role_name
+  lacework_aws_account_id = format("%d", var.lacework_aws_account_id)
+  use_existing_iam_role   = true
+  bucket_force_destroy    = true
 }
