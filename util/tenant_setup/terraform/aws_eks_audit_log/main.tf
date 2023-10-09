aaa41c0f8bb6848081ab1acc4d30c2243e394f95
ipcrm <matthew.cadorette@lacework.net>
2023-10-09 10:28:06 -0400

    fix(eks-audit-log): support customizing lacework account id

--- util/tenant_setup/terraform/aws_eks_audit_log/main.tf
+++ util/tenant_setup/terraform/aws_eks_audit_log/main.tf
@@ -13,6 +13,9 @@ variable "lacework_apikey" {}
 variable "lacework_apisecret" {}
 variable "cluster_name" {}
 variable "cluster_region" {}
+variable "lacework_aws_account_id" {
+  default = "434813966438"
+}
 
 provider "lacework" {
   subaccount = var.lacework_subaccount
@@ -26,9 +29,10 @@ provider "aws" {
 }
 
 module "aws_eks_audit_log" {
-  source               = "lacework/eks-audit-log/aws"
-  version              = "0.4.2"
-  cloudwatch_regions   = [var.cluster_region]
-  cluster_names        = [var.cluster_name]
-  bucket_force_destroy = true
+  lacework_aws_account_id = var.lacework_aws_account_id
+  source                  = "lacework/eks-audit-log/aws"
+  version                 = "0.4.2"
+  cloudwatch_regions      = [var.cluster_region]
+  cluster_names           = [var.cluster_name]
+  bucket_force_destroy    = true
 }
