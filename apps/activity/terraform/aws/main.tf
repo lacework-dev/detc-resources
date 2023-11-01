5ad9bc4b1e33e6283c9d1c3c80f5e24c10bfb926
Gabe O'Brien <gabe.obrien@lacework.net>
2023-11-01 11:41:08 -0700

    fix: aws cloud activity uses role attached to ec2 instance instead of role with credentials (#142)

--- apps/activity/terraform/aws/main.tf
+++ apps/activity/terraform/aws/main.tf
@@ -1,23 +1,36 @@
-variable "user_name" {}
+variable "role_name" {}
+variable "profile_name" {}
 
-resource "aws_iam_user" "aws_user" {
-  name = "${var.user_name}-user"
-  path = "/system/"
+resource "aws_iam_role" "cloud_activity_role" {
+  name = "${var.role_name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
   tags = {
-    tag-key = "tag-value"
+      tag-key = "tag-value"
   }
 }
 
-resource "aws_iam_access_key" "access_key" {
-  user = aws_iam_user.aws_user.name
+resource "aws_iam_instance_profile" "cloud_activity_profile" {
+  name = "${var.profile_name}"
+  role = "${var.role_name}"
 }
 
-output "secret_key" {
-  value = aws_iam_access_key.access_key.secret
+output "cloud_activity_profile_arn" {
+  value = aws_iam_instance_profile.cloud_activity_profile.name
   sensitive = true
-}
-
-output "access_key" {
-  value = aws_iam_access_key.access_key.id
 }
