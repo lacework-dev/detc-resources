07abace78176959261d1d438834e626aa41bf2fe
ipcrm <matthew.cadorette@lacework.net>
2023-12-06 14:23:20 -0500

    fix: gcp audit log integration use pubsub

--- apps/lacework-accounts/gcp/main.tf
+++ apps/lacework-accounts/gcp/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     lacework = {
       source  = "lacework/lacework"
-      version = "1.0.1"
+      version = "1.17.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "4.36.0"
+      version = ">= 4.4.0"
     }
   }
 }
@@ -23,18 +23,13 @@ provider "lacework" {
   api_secret = var.lacework_apisecret
 }
 
-provider "google" {}
-
 module "gcp_project_level_config" {
   source  = "lacework/config/gcp"
-  version = "2.4.2"
+  version = "2.4.3"
 }
 
-module "gcp_project_audit_log" {
-  source                       = "lacework/audit-log/gcp"
-  version                      = "3.4.2"
-  service_account_name         = module.gcp_project_level_config.service_account_name
-  service_account_private_key  = module.gcp_project_level_config.service_account_private_key
-  use_existing_service_account = true
-  bucket_force_destroy         = true
+module "gcp_project_level_pub_sub_audit_log" {
+  source           = "lacework/pub-sub-audit-log/gcp"
+  version          = "0.4.0"
+  integration_type = "PROJECT"
 }
