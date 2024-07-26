2381ba7b9e8c7926f06b3524cbb5e7bad870da3d
Gabe O'Brien <gabe.obrien@lacework.net>
2024-07-26 09:06:06 -0700

    fix: EKS clusters are now all version  1.29 which has support till 03/25/2025

--- extensions/eks/main.tf
+++ extensions/eks/main.tf
@@ -86,7 +86,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "18.26.5"
   cluster_name    = local.cluster_name
-  cluster_version = "1.23"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
