3a030a16528b0079773cb3367a1bf03c93613145
Gabe O'Brien <gabe.obrien@lacework.net>
2025-06-15 21:33:49 -0700

    fix: bump ubuntu os version

--- extensions/gcpcompute/main.tf
+++ extensions/gcpcompute/main.tf
@@ -53,7 +53,7 @@ resource "google_compute_instance" "instance-server" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
